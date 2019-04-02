a75503c69e3f49c352750beb8af9d3b3368895a1
Dan Carter <djcarter85@gmail.com>
2019-04-02 22:34:13 +0100

    Add code to run match simulator to program

--- FootballPredictor/Program.cs
+++ FootballPredictor/Program.cs
@@ -1,15 +1,48 @@
 ﻿namespace FootballPredictor
 {
     using System;
-    using System.Threading.Tasks;
 
     public static class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            await FootballDataUpdater.UpdateAsync();
+            var homeTeamName = "Man City";
+            var awayTeamName = "Huddersfield";
+            var simulations = 20;
+
+            var simulationResult = MatchSimulator.Simulate(homeTeamName, awayTeamName, simulations);
+
+            Console.WriteLine($"{homeTeamName} v {awayTeamName}");
+            Console.WriteLine($"Expected: {simulationResult.ExpectedScore.ExpectedHomeGoals:N2}-{simulationResult.ExpectedScore.ExpectedAwayGoals:N2}");
+            Console.WriteLine();
+
+            Console.WriteLine("Simulated:");
+            foreach (var score in simulationResult.SampleScores)
+            {
+                Console.WriteLine(GetScoreDescription(score));
+            }
 
             Console.ReadLine();
+        }
+
+        private static string GetScoreDescription(Score score)
+        {
+            return $"{GetResultDescription(score.Result)} {score.HomeScore}-{score.AwayScore}";
+        }
+
+        private static string GetResultDescription(Result result)
+        {
+            switch (result)
+            {
+                case Result.HomeWin:
+                    return "***    ";
+                case Result.Draw:
+                    return " *   * ";
+                case Result.AwayWin:
+                    return "    ***";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(result), result, null);
+            }
         }
     }
 }
