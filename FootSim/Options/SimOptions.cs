8f46298ed21e66ffe0e03422a7ad44cfb3428f41
Dan Carter <djcarter85@gmail.com>
2019-04-05 21:32:57 +0100

    Options ordering

--- FootSim/Options/SimOptions.cs
+++ FootSim/Options/SimOptions.cs
@@ -9,6 +9,13 @@
     {
         private static readonly LocalDatePattern Pattern = LocalDatePattern.Iso;
 
+        [Option('l', "league", Required = true,
+            HelpText = "The league to run the simulation for. Supports \"epl\" (English Premier League), \"champ\" (English Championship), \"l1\" (English League One), \"l2\" (English League Two), \"conf\" (English Conference).")]
+        public League League { get; set; }
+
+        [Option('s', "season", Required = true, HelpText = "The season to run the simulation for. Denoted by the year in which the season starts, e.g. \"2018\" or \"18\" for 2018-2019.")]
+        public int Season { get; set; }
+
         [Option('o', "on", Required = false, HelpText = "Date on which to perform the simulation. Format yyyy-MM-dd.")]
         public string OnString { get; set; }
 
@@ -16,12 +23,5 @@
 
         [Option('t', "times", Required = false, Default = 10_000, HelpText = "The number of times to simulate the season.")]
         public int Times { get; set; }
-
-        [Option('s', "season", Required = true, HelpText = "The season to run the simulation for. Denoted by the year in which the season starts, e.g. \"2018\" or \"18\" for 2018-2019.")]
-        public int Season { get; set; }
-
-        [Option('l', "league", Required = true, 
-            HelpText = "The league to run the simulation for. Supports \"epl\" (English Premier League), \"champ\" (English Championship), \"l1\" (English League One), \"l2\" (English League Two), \"conf\" (English Conference).")]
-        public League League { get; set; }
     }
 }
