4c1e75e37d8313fddfe8858a789cb27bf5ca939f
Dan Carter <djcarter85@gmail.com>
2019-04-06 17:33:37 +0100

    Add CSV file for league names

---
+++ FootSim.Core/CsvEmbeddedResources.cs
@@ -0,0 +1,13 @@
+﻿namespace FootSim.Core
+{
+    using System.IO;
+    using System.Reflection;
+
+    public static class CsvEmbeddedResources
+    {
+        public static Stream GetStream(string fileName)
+        {
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream($"FootSim.Core.{fileName}");
+        }
+    }
+}
--- FootSim.Core/League.cs
+++ FootSim.Core/League.cs
@@ -4,16 +4,16 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Reflection;
     using CsvHelper;
 
     public class League
     {
         private readonly Lazy<IReadOnlyList<PositionGrouping>> positionGroupingsLazy;
+        private readonly Lazy<string> leagueNameLazy;
 
         private IReadOnlyList<PositionGrouping> FetchPositionGroupings()
         {
-            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FootSim.Core.PositionGroupings.csv"))
+            using (Stream stream = CsvEmbeddedResources.GetStream("PositionGroupings.csv"))
             {
                 using (var textReader = new StreamReader(stream))
                 {
@@ -21,10 +21,31 @@
                     {
                         return csvReader.GetRecords<CsvPositionGrouping>()
                             .Where(cpg =>
-                                cpg.Nation == this.Nation.ToString() && cpg.Tier == this.Tier &&
+                                cpg.Nation == this.Nation.ToString() &&
+                                cpg.Tier == this.Tier &&
                                 cpg.StartingYear == this.StartingYear)
                             .Select(cpg => new PositionGrouping(cpg.LongName, cpg.ShortName, cpg.Min, cpg.Max))
                             .ToArray();
+                    }
+                }
+            }
+        }
+
+        private string FetchLeagueName()
+        {
+            using (Stream stream = CsvEmbeddedResources.GetStream("LeagueNames.csv"))
+            {
+                using (var textReader = new StreamReader(stream))
+                {
+                    using (var csvReader = new CsvReader(textReader))
+                    {
+                        var csvLeagueName = csvReader.GetRecords<CsvLeagueName>()
+                            .SingleOrDefault(cln =>
+                                cln.Nation == this.Nation.ToString() &&
+                                cln.Tier == this.Tier &&
+                                cln.Contains(this.StartingYear));
+
+                        return csvLeagueName?.Name ?? $"Tier {this.Tier}";
                     }
                 }
             }
@@ -47,6 +68,22 @@
             public int Max { get; set; }
         }
 
+        private class CsvLeagueName
+        {
+            public string Nation { get; set; }
+
+            public int Tier { get; set; }
+
+            public int? StartYear { get; set; }
+
+            public int? EndYear { get; set; }
+
+            public string Name { get; set; }
+
+            public bool Contains(int year) =>
+                (this.StartYear == null || this.StartYear <= year) && (this.EndYear == null || year < this.EndYear);
+        }
+
         public League(Nation nation, int tier, int startingYear)
         {
             this.Nation = nation;
@@ -54,6 +91,7 @@
             this.StartingYear = startingYear;
 
             this.positionGroupingsLazy = new Lazy<IReadOnlyList<PositionGrouping>>(this.FetchPositionGroupings);
+            this.leagueNameLazy = new Lazy<string>(this.FetchLeagueName);
         }
 
         public Nation Nation { get; }
@@ -62,80 +100,12 @@
 
         public int StartingYear { get; }
 
-        public string Description => $"{this.TierDescription} ({this.Nation})";
+        public string Description => $"{this.LeagueName} ({this.Nation})";
 
         public string EditionDescription => $"{this.StartingYear}-{this.StartingYear + 1}";
 
         public IEnumerable<PositionGrouping> PositionGroupings => this.positionGroupingsLazy.Value;
 
-        private string TierDescription
-        {
-            get
-            {
-                switch (this.Nation)
-                {
-                    case Nation.England:
-                        switch (this.Tier)
-                        {
-                            case 0:
-                                return "Premier League";
-                            case 1:
-                                return "Championship";
-                            case 2:
-                                return "League One";
-                            case 3:
-                                return "League Two";
-                            case 4:
-                                return "National League";
-                        }
-
-                        break;
-                    case Nation.Germany:
-                        switch (this.Tier)
-                        {
-                            case 0:
-                                return "Bundesliga 1";
-                            case 1:
-                                return "Bundesliga 2";
-                        }
-
-                        break;
-                    case Nation.Italy:
-                        switch (this.Tier)
-                        {
-                            case 0:
-                                return "Serie A";
-                            case 1:
-                                return "Serie B";
-                        }
-
-                        break;
-                    case Nation.Spain:
-                        switch (this.Tier)
-                        {
-                            case 0:
-                                return "La Liga Primera";
-                            case 1:
-                                return "La Liga Segunda";
-                        }
-
-                        break;
-                    case Nation.France:
-                        switch (this.Tier)
-                        {
-                            case 0:
-                                return "Ligue 1";
-                            case 1:
-                                return "Ligue 2";
-                        }
-
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                throw new ArgumentOutOfRangeException();
-            }
-        }
+        private string LeagueName => this.leagueNameLazy.Value;
     }
 }
