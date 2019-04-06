e23a826f33089feb41af79a7e4a2f3d32761a4f5
Dan Carter <djcarter85@gmail.com>
2019-04-06 17:51:36 +0100

    Add CSV for file names

--- FootSim.Core/League.cs
+++ FootSim.Core/League.cs
@@ -10,6 +10,7 @@
     {
         private readonly Lazy<IReadOnlyList<PositionGrouping>> positionGroupingsLazy;
         private readonly Lazy<string> leagueNameLazy;
+        private readonly Lazy<string> fileNameLazy;
 
         private IReadOnlyList<PositionGrouping> FetchPositionGroupings()
         {
@@ -51,6 +52,25 @@
             }
         }
 
+        private string FetchFileName()
+        {
+            using (Stream stream = CsvEmbeddedResources.GetStream("FileNames.csv"))
+            {
+                using (var textReader = new StreamReader(stream))
+                {
+                    using (var csvReader = new CsvReader(textReader))
+                    {
+                        var csvFileName = csvReader.GetRecords<CsvFileName>()
+                            .Single(cfn =>
+                                cfn.Nation == this.Nation.ToString() &&
+                                cfn.Tier == this.Tier);
+
+                        return csvFileName.FileName;
+                    }
+                }
+            }
+        }
+
         private class CsvPositionGrouping
         {
             public string Nation { get; set; }
@@ -84,6 +104,15 @@
                 (this.StartYear == null || this.StartYear <= year) && (this.EndYear == null || year < this.EndYear);
         }
 
+        private class CsvFileName
+        {
+            public string Nation { get; set; }
+
+            public int Tier { get; set; }
+
+            public string FileName { get; set; }
+        }
+
         public League(Nation nation, int tier, int startingYear)
         {
             this.Nation = nation;
@@ -92,6 +121,7 @@
 
             this.positionGroupingsLazy = new Lazy<IReadOnlyList<PositionGrouping>>(this.FetchPositionGroupings);
             this.leagueNameLazy = new Lazy<string>(this.FetchLeagueName);
+            this.fileNameLazy = new Lazy<string>(this.FetchFileName);
         }
 
         public Nation Nation { get; }
@@ -105,6 +135,8 @@
         public string EditionDescription => $"{this.StartingYear}-{this.StartingYear + 1}";
 
         public IEnumerable<PositionGrouping> PositionGroupings => this.positionGroupingsLazy.Value;
+
+        public string FileName => this.fileNameLazy.Value;
 
         private string LeagueName => this.leagueNameLazy.Value;
     }
