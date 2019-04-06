7c2c470a78c97cf492377e0f3a0373268c065245
Dan Carter <djcarter85@gmail.com>
2019-04-06 22:06:08 +0100

    Allow start year to be optional

--- FootSim/Options/UpdateOptions.cs
+++ FootSim/Options/UpdateOptions.cs
@@ -13,8 +13,8 @@
         [Value(1, Required = true, HelpText = "The 0-based index of the league within the nation's football pyramid.")]
         public int Tier { get; set; }
 
-        [Value(2, Required = true, HelpText = "The year in which the season starts, e.g. \"2018\" or \"18\" for 2018-2019.")]
-        public int StartingYear { get; set; }
+        [Value(2, Required = false, HelpText = "The year in which the season starts, e.g. \"2018\" or \"18\" for 2018-2019. Defaults to the current season.")]
+        public int? StartingYear { get; set; }
 
         public League League => new League(Conversions.ToNation(this.Nation), this.Tier, Conversions.ToStartingYear(this.StartingYear));
 
